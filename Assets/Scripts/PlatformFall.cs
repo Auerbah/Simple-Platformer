2da60f9ffdd0c5e8bd5514dfcdff6cd6f569016f
auerbah <kvasnyy.alexey.borisovich@gmail.com>
2019-01-09 17:52:38 +0300

    Initial Unity commit

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+
+	public float fallFelay = 1f;
+
+	private Rigidbody2D rb2d;
+	
+	void Start ()
+	{
+
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Invoke("Fall", fallFelay);
+		}
+	}
+
+	void Fall()
+	{
+		rb2d.bodyType = RigidbodyType2D.Dynamic;
+	}
+}
